13d7095d0df4143c702e124f881e1397800544b7
bernardrayoso <rayosobernard@gmail.com>
2022-02-16 12:24:14 +0800

    Update Link Router
    Update Link Router navigation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,12 +7,18 @@ const Header = () => {
     <header>
 <Navbar bg="dark" variant='dark' expand="lg">
   <Container>
-    <Navbar.Brand href="/">EShop</Navbar.Brand>
+    <LinkContainer to="/">
+      <Navbar.Brand >EShop</Navbar.Brand>
+    </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav"  className="justify-content-end" >
+    <Navbar.Collapse id='basic-navbar-nav'  className='justify-content-end'>
       <Nav>
-        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-        <Nav.Link href="/login"><i className='fas fa-user'></i> Sign in</Nav.Link>  
+        <LinkContainer to='/cart'>
+          <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/login'>
+          <Nav.Link><i className='fas fa-user'></i> Sign in</Nav.Link>  
+        </LinkContainer>
       </Nav>
     </Navbar.Collapse>
   </Container>
