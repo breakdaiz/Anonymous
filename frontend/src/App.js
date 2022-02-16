ef12a9d035f3c2c22b705525b9c870263cc924c0
bernardrayoso <rayosobernard@gmail.com>
2022-02-16 12:22:49 +0800

    Update codes for integration of router
    - Update codes for integration of react router
    - Integrate Product screen
    - Integrate Product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Container  } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/Homescreen';
-import About from './screens/About';
 import { BrowserRouter , Routes, Route,   } from 'react-router-dom'
 
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Screens
+import HomeScreen from './screens/Homescreen';
+import About from './screens/About';
+import ProductScreen from './screens/ProductScreen';
+import ProductScreenDetail  from './screens/ProductScreenDetail';
+// import ProductSummary  from './screens/ProductSummary';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-    <main className='py-3'>  
-      <Container>
-        <Routes>
-            <Route index element={<HomeScreen />} />
-            <Route  path='/about' element={<About/>} />
-        </Routes>
-      
-      </Container>
-    </main>  
-    <Footer/>
+     <Header/>
+      <main className='py-3'>  
+        <Container>
+          <Routes>
+              <Route index element={<HomeScreen />} />
+              <Route path='about' element={<About/>} />
+            
+              <Route path="product" element={<ProductScreenDetail />}>
+                  <Route path=":productId" element={<ProductScreen />} />
+                  {/* <Route path="sent" element={<ProductSummary />} /> */}
+              </Route>
+          </Routes>
+        </Container>
+      </main>   
+     <Footer/>
     </BrowserRouter>
   ); 
 }
