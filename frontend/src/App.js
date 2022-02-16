9b0c91f32c5a41691817212851a3bbca14fa7332
bernardrayoso <rayosobernard@gmail.com>
2022-02-16 10:06:28 +0800

    Integrate React Router
    Integrate React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import { Container  } from 'react-bootstrap' 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/Homescreen'
-import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom'
+import React from 'react';
+import { Container  } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/Homescreen';
+import About from './screens/About';
+import { BrowserRouter , Routes, Route,   } from 'react-router-dom'
 
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
     <Header/>
     <main className='py-3'>  
       <Container>
-        <HomeScreen />
+        <Routes>
+            <Route index element={<HomeScreen />} />
+            <Route  path='/about' element={<About/>} />
+        </Routes>
+      
       </Container>
-      </main>  
+    </main>  
     <Footer/>
-    </>
+    </BrowserRouter>
   ); 
 }
 export default App;
