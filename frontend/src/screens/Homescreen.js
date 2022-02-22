547c49e0efb3d612526026dac053402a5f6c4cc2
Ryu <rayosobernard@gmail.com>
2022-02-22 20:35:58 +0800

    Refactor code
    Refactor code to apply Redux on API calls and Route

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import Product from '../components/Product'
-import axios from 'axios';
+import Product from "../components/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-    const [products, setProducts ] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            console.log("data",data);
-            setProducts(data)
-        }
-        fetchProducts();
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-        
-    }, [])
+  //   const products = [];
 
-    return ( 
-        <> 
-        <h1> Latest Products </h1> 
+  return (
+    <>
+      <h1> Latest Products </h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error} </Message>
+      ) : (
         <Row>
-            {products.map(product=>(
-                <Col sm={12} md={6} lg={4} xl={3} >
-                    <Product product={product} />
-                </Col>
-            ))}
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
         </Row>
-        
-        </>
-    )
-} 
+      )}
+    </>
+  );
+};
 
-export default HomeScreen 
+export default HomeScreen;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,92 +1,101 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { listProductDetails } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
-import { Col, Row, Image, ListGroup, Card, Button, ListGroupItem  } from "react-bootstrap";
+import {
+  Col,
+  Row,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 // import products from "../products";
-import axios  from "axios";
-
-
 
 const ProductScreen = () => {
+  const match = { params: useParams() };
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-    const match  = {params: useParams()}
-    const [product, setProduct] = useState({});
-    
-    useEffect(()=> {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data);
-        }
-        fetchProduct();
+  useEffect(
+    () => {
+      dispatch(listProductDetails(match.params.id));
+    },
+    dispatch,
+    [match]
+  );
 
-    }, [match])
+  return (
+    <>
+      <Link className='btn btn-light my-3' to={`/`}>
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <h3>{product.name}</h3>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroupItem>
+              <ListGroupItem>Price: ${product.price}</ListGroupItem>
+              <ListGroupItem>Description: ${product.description}</ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
 
+                <ListGroupItem className='d-grid gap-2'>
+                  <Button
+                    variant='primary'
+                    className='btn-block'
+                    type='button'
+                    disabled={product.countInStock === 0}>
+                    Add to cart
+                  </Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+};
 
-    return (
-        <>
-            <Link className='btn btn-light my-3' to={`/`}>
-                    Go Back 
-            </Link>
-            <Row>
-                <Col md={6}>
-                    <Image  src={product.image} alt={product.name} fluid />
-                </Col> 
-                <Col md={3}>
-                    <ListGroup variant="flush">
-                        <ListGroupItem>
-                            <h3>{product.name}</h3>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <Rating 
-                             value={product.rating}
-                             text={`${product.numReviews} reviews`}
-                             />
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Price: ${product.price}                           
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            Description: ${product.description}                           
-                        </ListGroupItem>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroupItem>
-                                <Row>
-                                    <Col>
-                                    Price:
-                                    </Col>
-                                    <Col>
-                                    <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Row>
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroupItem>
-
-                            <ListGroupItem className="d-grid gap-2">
-                                <Button variant="primary" className='btn-block' type='button' disabled={product.countInStock === 0 }> 
-                                    Add to cart
-                                </Button>
-                            </ListGroupItem>      
-                                   
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    )
-}
-
-export default ProductScreen
+export default ProductScreen;
